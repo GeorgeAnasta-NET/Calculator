6a7489771b541303d57df8ef64c726e74edf0485
George Anastasiadis <anastasiadisgeo87@gmail.com>
2023-04-05 14:42:18 +0300

    Add project files.

---
+++ Calc/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Calc
+{
+    public class Calculator
+    {
+        public static double Addition(double firstNum, double secondNum)
+        {
+            double result = firstNum + secondNum;
+            return result;
+        }
+        public static double Substaction(double firstNum, double secondNum)
+        {
+            double result = firstNum - secondNum;
+            return result;
+        }
+        public static double Multiplication(double firstNum, double secondNum)
+        {
+            double result = firstNum * secondNum;
+            return result;
+        }
+        public static double Division(double firstNum, double secondNum)
+        {
+             double result = firstNum / secondNum;
+             return result;
+        }
+    }
+}
---
+++ Controllers/CalculateController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculateController : Controller
+    {
+        // GET: Calculate
+        public ActionResult Calculate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(string firstNumber, string secondNumber, string Cal)
+        {
+            double firstNum = string.IsNullOrEmpty(firstNumber) ? 0 : Convert.ToInt32(firstNumber);
+            double result = 0;
+            double secondNum = 0;
+            
+            if (Cal != "sqrt" || Cal != ("x^2"))
+                secondNum = string.IsNullOrEmpty(secondNumber) ? 0 : Convert.ToInt32(secondNumber);
+            
+            switch (Cal)
+            {
+                case "+":
+                    result = Calc.Calculator.Addition(firstNum, secondNum);
+                    break;
+                case "-":
+                    result = Calc.Calculator.Substaction(firstNum, secondNum);
+                    break;
+                case "*":
+                    result = Calc.Calculator.Multiplication(firstNum, secondNum);
+                    break;
+                case "/":
+                    result = Calc.Calculator.Division(firstNum, secondNum);
+                    break;
+                case ("sqrt"):
+                    result = Math.Sqrt(firstNum);
+                    break;
+                case ("x^2"):
+                    result = Calc.Calculator.Multiplication(firstNum, firstNum);
+                    break;
+            }
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
